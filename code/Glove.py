40e340532ac028f7c3895397b65145ea98c0fdb1
Orry Despo <oriondespo@yahoo.com>
2015-11-23 16:52:46 -0800

    Refactored weighted vsm model

--- code/Glove.py
+++ code/Glove.py
@@ -66,29 +66,14 @@ class Glove:
         return targetvec
 
     # Computes the average of the glove vectors of all elements in words
-    def getAverageVec(self, words):
+    # If unigram model is passed in, weights each word by its inverse unigram probability
+    def getAverageVec(self, words, unigrams = None):
         targetvec = [0]*len(self.vectors.values()[0])
         count = 0;
         for word in words:
             wordvec = self.getVec(word);
             if(wordvec != None):
-                count += 1;
-                targetvec = map(lambda i: targetvec[i] + wordvec[i], xrange(len(targetvec)));
-                
-            else:
-                if(self.v): self.error("Glove does not have \"" + word + "\" in its vocabulary");
-
-        if(count == 0): return None;
-        return map(lambda x: x/count, targetvec);
-
-    # Computes the average of the glove vectors of all elements in words. However, weights each word by its inverse unigram probability
-    def getWeightedAverageVec(self, words, unigrams):
-        targetvec = [0]*len(self.vectors.values()[0])
-        count = 0;
-        for word in words:
-            wordvec = self.getVec(word);
-            if(wordvec != None):
-                wordProb = unigrams.getSingleScore(word)
+                wordProb = 1 if unigrams == None else unigrams.getSingleScore(word)
                 count += 1;
                 targetvec = map(lambda i: targetvec[i] + wordvec[i]*(1/wordProb), xrange(len(targetvec)));
                 
